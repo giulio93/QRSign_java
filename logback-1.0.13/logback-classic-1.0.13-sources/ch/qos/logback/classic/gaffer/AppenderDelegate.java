a53e6b13458ef4da5573ef84aefb696662f77ec5
Giulio <pilotto.giulio@gmail.com>
2017-05-09 16:27:53 +0200

    First Commit

---
+++ logback-1.0.13/logback-classic-1.0.13-sources/ch/qos/logback/classic/gaffer/AppenderDelegate.java
@@ -0,0 +1,23 @@
+package ch.qos.logback.classic.gaffer;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class AppenderDelegate
+  extends ch.qos.logback.classic.gaffer.ComponentDelegate  implements
+    groovy.lang.GroovyObject {
+public AppenderDelegate
+(ch.qos.logback.core.Appender appender) {
+super (null);
+}
+public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+public  java.lang.Object getProperty(java.lang.String property) { return null;}
+public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public  java.lang.String getLabel() { return (java.lang.String)null;}
+}
